d9f7de5f59c4a9ec55e7ff6705b0ea4b495f8884
DenisVashkevich <vdenis.biocom@gmail.com>
2016-07-07 00:54:18 +0300

    reminder for tomorrow

---
+++ SmallThings/SmallThings/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmallThings.Interfaces;
+
+namespace SmallThings.Services
+{
+	public class ProductService
+	{
+		private readonly IProductApiService _productApiService;
+		private readonly IProductProcessingStrategy _productProcessingStrategy;
+
+		public ProductService(IProductApiService productApiService, IProductProcessingStrategy productProcessingStrategy)
+		{
+			_productProcessingStrategy = productProcessingStrategy;
+			_productApiService = productApiService;
+		}
+
+	}
+}
